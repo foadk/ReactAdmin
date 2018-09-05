028aeaed6e1da9463a1e2fd9bb5bb8d89b9fd317
Foad Khaleghi <khaleghi.foad@gmail.com>
2018-09-05 12:08:10 +0430

    change layout folder structure

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Navbar from './sections/Navbar';
-import Sidebar from './sections/Sidebar';
-import Main from './sections/Main';
-import Footer from './sections/Footer';
+import Navbar from './sections/Navbar/Navbar';
+import Sidebar from './sections/Sidebar/Sidebar';
+import Main from './sections/Main/Main';
+import Footer from './sections/Footer/Footer';
 
 class Layout extends Component {
     render() {
---
+++ src/Layout/sections/Sidebar/Sidebar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import navigationItems from '../../../_nav';
+
+class Sidebar extends Component {
+
+    itemClickHandler = (event) => {
+        if (event.target.classList.contains("nav-dropdown-toggle")) {
+            event.preventDefault();
+            event.target.parentElement.classList.toggle("open");
+        }
+    }
+
+    buildNavigation = (navItems) => {
+        const result = navItems.map(item => {
+            if (item.title === true) {
+                return (<li key={item.name} className="nav-title">{item.name}</li>);
+            } else if (item.hasOwnProperty('divider')) {
+                return (<li key={item.name} className="divider"></li>);
+            } else {
+                let res = (
+                    <li key={item.name} className={"nav-item" + (item.hasOwnProperty('children') ? " nav-dropdown" : "")}>
+                        <a className={"nav-link" + (item.hasOwnProperty('children') ? " nav-dropdown-toggle" : "") +
+                            (item.hasOwnProperty('variant') ? " nav-link-" + item.variant : "")}
+                            onClick={this.itemClickHandler}
+                            href={!item.hasOwnProperty('children') ? item.url : "#"}>
+                            <i className={item.icon} ></i>
+                            {item.name}
+                            {item.hasOwnProperty('badge') ?
+                                <span className={"tag tag-" + item.badge.variant}>{item.badge.text}</span> : ''}
+                        </a>
+                        {item.hasOwnProperty('children') ?
+                            <ul className="nav-dropdown-items">{this.buildNavigation(item.children)}</ul> : ''}
+                    </li>
+                );
+                return res;
+            }
+        });
+        return result;
+    }
+
+    render() {
+        const navigaiton = this.buildNavigation(navigationItems.items);
+        return (
+            <div className="sidebar">
+                <nav className="sidebar-nav">
+                    <ul className="nav">
+                        {navigaiton}
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
