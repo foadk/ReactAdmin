9dc6a22da67420429b36ad74cf5220dfa91d7d98
Foad Khaleghi <khaleghi.foad@gmail.com>
2018-10-07 11:57:16 +0330

    fix a validation bug

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,7 +5,7 @@ export const checkValidity = (value, rules) => {
     let isValid = true;
 
     if (rules.required) {
-        isValid = isValid && (value.trim() !== '');
+        isValid = isValid && (String(value).trim() !== '');
     }
     if (rules.minLength) {
         isValid = isValid && (value.length >= rules.minLength);
